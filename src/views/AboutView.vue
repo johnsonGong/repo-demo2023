d9236313bed4427790125c7499db022ca66aebcb
gonglong <gong@gong.com>
2023-09-30 12:49:43 +0800

    init 初始化项目
    使用VUE3官方脚手架

---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <div>
+      <h1>This is an about page</h1>
+      <p> Always remember that you are absolutely unique. Just like everyone else. </p>
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
